dbe74e1b1523262787bc085cd3d1f06e3a320ebc
Shlok Kyal <shlok.kyal@gmail.com>
2021-10-16 00:31:08 +0530

    Changes Made

--- codeeditor-frontend/src/components/DropList.js
+++ codeeditor-frontend/src/components/DropList.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import {Link,useHistory} from 'react-router-dom'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const DropList = () => {
-    const history=useHistory();
-    const click=()=>{
-        localStorage.removeItem('auth-token');
-        history.push('/login');
-    }
-    return (
-        <div className="dropdown-content">
-            <Link to="/user" style={{textDecoration:'none',color:'black'}}><div>My Codes</div></Link>
-            <div onClick={click}>Logout</div>
-        </div>
-    )
-}
+  const history = useHistory();
+  const click = () => {
+    localStorage.removeItem("auth-token");
+    history.push("/login");
+    toast("Logged Out Successfully");
+  };
+  return (
+    <div className="dropdown-content">
+      <Link to="/user" style={{ textDecoration: "none", color: "black" }}>
+        <div>My Codes</div>
+      </Link>
+      <div onClick={click}>Logout</div>
+    </div>
+  );
+};
 
-export default DropList
+export default DropList;
