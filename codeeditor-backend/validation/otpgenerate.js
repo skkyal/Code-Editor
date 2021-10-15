2f85913870c09aeb98e1242337cd2a5eb12a9afb
Shlok Kyal <shlok.kyal@gmail.com>
2021-10-16 00:16:34 +0530

    Fixed Auth Errors

--- codeeditor-backend/validation/otpgenerate.js
+++ codeeditor-backend/validation/otpgenerate.js
@@ -1,5 +1,8 @@
 const nodemailer = require("nodemailer");
-module.exports = function (email) {
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+/*module.exports = function (email) {
   const otp = parseInt(Math.random() * 1000000);
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -30,6 +33,66 @@ module.exports = function (email) {
       console.log("Message sent:", info.messageId);
       console.log("Preview URL:", nodemailer.getTestMessageUrl(info));
     }
+  });
+
+  return otp;
+};*/
+
+module.exports = function (email) {
+  const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject("Failed to create access token :(");
+        }
+        resolve(token);
+      });
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.ACCEMAIL,
+        accessToken,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+      },
+    });
+
+    return transporter;
+  };
+  const otp = parseInt(Math.random() * 1000000);
+
+  const sendEmail = async (emailOptions) => {
+    try {
+      let emailTransporter = await createTransporter();
+      await emailTransporter.sendMail(emailOptions);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  sendEmail({
+    from: process.env.ACCEMAIL,
+    to: email,
+    subject: "Otp for registration is: ",
+    html:
+      "<h3>OTP for account verification is </h3>" +
+      "<h1 style='font-weight:bold;'>" +
+      otp +
+      "</h1>", // html body
   });
 
   return otp;
